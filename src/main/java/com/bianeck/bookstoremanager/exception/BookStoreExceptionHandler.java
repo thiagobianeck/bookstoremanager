ebd6192818bf2776493fc6b31f3ea726b154a912
Thiago Moreira Bianeck <thiagobianeck@gmail.com>
2020-12-14 00:26:54 -0300

    Added dependency for validation starter

--- src/main/java/com/bianeck/bookstoremanager/exception/BookStoreExceptionHandler.java
+++ src/main/java/com/bianeck/bookstoremanager/exception/BookStoreExceptionHandler.java
@@ -16,7 +16,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 @ControllerAdvice
 public class BookStoreExceptionHandler extends ResponseEntityExceptionHandler {
